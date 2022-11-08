e5a2de73484453592acaf2b7e02dcfb35fe4e193
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-09 00:20:26 +0600

    send singel  service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -33,6 +33,12 @@ async function run() {
             const cursor = servicesCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
         })
     } finally {
 
